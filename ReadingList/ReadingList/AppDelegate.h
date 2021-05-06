2193f1cdde56a61dc61d603f984d70cc5be3e13e
Jonathan Lehr <jonathan@aboutobjects.com>
2021-05-06 02:58:57 -0400

    Initial commit

---
+++ ReadingList/ReadingList/AppDelegate.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2021 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
