8b4b8916a069e4eaab36634789ee24f6e5dec8a2
Jonathan Lehr <jonathan@aboutobjects.com>
2021-05-05 22:27:04 -0400

    sendAction implementation

--- Coolness/Coolness/AppDelegate.m
+++ Coolness/Coolness/AppDelegate.m
@@ -7,6 +7,7 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     NSLog(@"In %s", __func__);
+    [UIApplication.sharedApplication sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
 }
 
 @end
